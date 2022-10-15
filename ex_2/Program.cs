f9149f5341a2376c437e49f6156af5b8908183d2
alpryakh <alpryakh@gmail.com>
2022-10-15 19:41:13 +0300

    2

---
+++ ex_2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Enter first number");
+double a= Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Enter second number");
+double b= Convert.ToDouble(Console.ReadLine());
+
+double max = a;
+if (max < b)
+{
+    max = b;
+    Console.WriteLine("max = "+b);
+    Console.WriteLine("min = "+a);
+}
+else 
+{
+    Console.WriteLine("max = "+a);
+    Console.WriteLine("min = "+b);
+}
