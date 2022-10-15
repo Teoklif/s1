e90b4b392dd7093b56e21021af59cb691fe8ec91
alpryakh <alpryakh@gmail.com>
2022-10-15 21:39:32 +0300

    8

---
+++ ex_8/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Enter:");
+int N = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+while (count < N+1)
+{
+if ( count % 2 == 0)
+{
+    Console.Write(count +", ");
+    count= count+2;
+}
+
+
+}
