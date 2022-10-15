f210ad03350e5db870e46c4d43c1aa07f0db5f74
alpryakh <alpryakh@gmail.com>
2022-10-15 20:00:00 +0300

    ex_4

---
+++ ex_4/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.WriteLine("Enter first number");
+double a= Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Enter second number");
+double b= Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Enter third number");
+double c= Convert.ToDouble(Console.ReadLine());
+
+
+double max = a;
+if (max < b)
+{
+    max = b;
+    if (max < c)
+    {
+        max = c;
+         Console.WriteLine("max = "+c);
+    }
+    else
+    {
+         Console.WriteLine("max = "+b);
+    }
+
+}
+else 
+{ if (max < c)
+    {
+        max = c;
+         Console.WriteLine("max = "+c);
+    }
+    else
+    {
+    
+    Console.WriteLine("max = "+a);
+
+}
+}
